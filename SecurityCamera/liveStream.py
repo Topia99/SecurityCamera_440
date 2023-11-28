d231ab81edd406634ce8246e53d9a19d4437af86
Topia99 <1242911982z@gmail.com>
2023-11-28 11:43:46 -0800

    Add camera live stream

---
+++ SecurityCamera/liveStream.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, Response
+import picamera
+import time
+import io
+import threading
+
+app = Flask(__name__)
+
+# Camera settings
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 24
+time.sleep(2) # Camera warm-up time
+
+def generate():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.framerate = 24
+        time.sleep(2) # Camera warm-up time
+
+        stream = io.BytesIO()
+        for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+            stream.seek(0)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
+            stream.seek(0)
+            stream.truncate()
+
+@app.route('/')
+def index():
+    return render_template('index.html') # Ensure you have an 'index.html' file in the 'templates' folder
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='10.66.45.179', port=8000, debug=True)
+
+
