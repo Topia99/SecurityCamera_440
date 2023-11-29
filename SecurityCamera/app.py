bdd16e11bfc399101f55ac05ef3cd50117ef3131
Topia99 <1242911982z@gmail.com>
2023-11-28 17:29:54 -0800

    go back to the simplest web hosting

--- SecurityCamera/app.py
+++ SecurityCamera/app.py
@@ -2,12 +2,21 @@ from flask import Flask, render_template, Response
 import picamera
 import time
 import io
-import threading
-import os
 
 app = Flask(__name__)
 
+# Global flag to track camera usage
+camera_in_use = False
+
 def generate():
+    global camera_in_use
+
+    if camera_in_use:
+        print("Camera already in use.")
+        return
+
+    camera_in_use = True
+
     with picamera.PiCamera() as camera:
         camera.resolution = (640, 480)
         camera.framerate = 24
@@ -21,6 +30,8 @@ def generate():
             stream.seek(0)
             stream.truncate()
 
+    camera_in_use = False
+
 @app.route('/')
 def index():
     return render_template('index.html')  # Ensure you have an 'index.html' file in the 'templates' folder
@@ -30,15 +41,6 @@ def video_feed():
     return Response(generate(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def run_server():
-    app.run(host='10.66.45.179', port=8000, debug=True, use_reloader=False)
-
-def listen_for_exit():
-    while True:
-        if input() == 'q':
-            print("Exiting...")
-            os._exit(0)
-
 if __name__ == '__main__':
-    threading.Thread(target=run_server).start()
-    listen_for_exit()
+    app.run(host='10.66.45.45.179', port=8000, debug=True)
+
